b420ac20e69883db158d01adde7ce36d55585ebc
rendnil <andrew.lindner.15@gmail.com>
2018-10-09 16:55:16 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+// Code Keypad Component Here
+import React from 'react';
+
+
+  export default class EyesOnMe extends React.Component{
+
+    handleBlur = () =>{
+      console.log('Hey! Eyes on me!');
+    }
+
+    handleFocus = () => {
+      console.log("Good!");
+    }
+
+
+    render(){
+      return(
+        <button
+        onBlur={this.handleBlur}
+        onFocus = {this.handleFocus}
+        >Hey</button>
+      )
+
+    }
+
+
+  }
